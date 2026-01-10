08f5a8eaa3040f30b0e5acdde20e231cdb526ffc
NizarDabouqi <nizar.dabouqi133@gmail.com>
2026-01-10 22:25:30 +0300

    Add Spring Boot backend and Angular frontend source

---
+++ src/test/java/com/action/actionelectronics/ActionElectronicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.action.actionelectronics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionElectronicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
